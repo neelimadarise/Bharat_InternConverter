fe77754ca40bb4180c0b483cee9bf1278f7e1aed
neelimadarise <128125396+neelimadarise@users.noreply.github.com>
2023-10-08 17:15:25 +0530

    Add files via upload

---
+++ converterclass.java
@@ -0,0 +1,17 @@
+package com.example.converter;
+
+public class converterclass {
+    public double c2f_fn(double degCel) {
+
+        double celsius = 0;
+        return (celsius*1.8)+32;
+
+    }
+
+    public double f2c_fn(double degFar) {
+
+        double farenheit = 0;
+        return (farenheit-32)/1.8;
+
+    }
+}
